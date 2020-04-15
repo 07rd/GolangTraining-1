598bed5a50168fccb9d881979c9e5b6f9712080d
devendra058 <58697457+devendra058@users.noreply.github.com>
2020-04-15 14:59:08 +0530

    Assignment for Day1

---
+++ Day1/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Assignment 1")
+	fmt.Println("Hello World!")
+	fmt.Println("===============================================\n")
+	fmt.Println("Assignment 2")
+	pyramid(5, true)
+	fmt.Println("Assignment 3")
+	pyramid(5, false)
+	fmt.Println("Assignment 4")
+	factorialNum(5)
+}
+
+func pyramid(rowNumber int, astric bool) {
+
+	if (astric) {
+		for row := 0; row < rowNumber; row++ {
+			for col := 0; col <= row; col++ {
+				fmt.Print("* ")
+			}
+			fmt.Println()
+		}
+	} else {
+		var num int = 1
+		for row := 0; row < rowNumber; row++ {
+			for col := 0; col <= row; col++ {
+				fmt.Print(fmt.Sprint(num, " "))
+				num++
+			}
+			fmt.Println()
+		}	
+	}
+	fmt.Println("===============================================\n")
+}
+
+func factorialNum(num int) {
+	var value = 1
+	for i := 1; i <= num; i++ {
+			value *= i
+		}
+	fmt.Println(fmt.Sprint("Factorial of number ", num, " is ", value))
+}
